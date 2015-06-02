75fb54e604a2fda5da035feb3ab904f37b34cb33
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-06-02 16:23:23 -0500

    Update base64.c

--- base64.c
+++ base64.c
@@ -8,7 +8,7 @@
 
 /*************************** HEADER FILES ***************************/
 #include <stdlib.h>
-#include <base64.h>
+#include <crypto.h>
 
 /****************************** MACROS ******************************/
 #define NEWLINE_INVL 76
